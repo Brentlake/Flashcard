d2c9b1c2b9ea5ac5641224d9bca26f3196fe72ab
Brenton Lake <Brenton Lake>
2017-01-25 18:14:09 -0600

    not finished was having a lot of trouble got help and am going to finish and push final as soons as possible

---
+++ flashcard.js
@@ -0,0 +1,30 @@
+var inquirer = require("inquirer");
+var basicJSON = require('./basic.json');
+
+console.log(basicJSON);
+
+
+
+// function BasicCard{
+//   parse(basicJSON)
+// }
+// BasicCard.prototype.printInfo = function() {
+ 
+
+// };
+
+
+
+
+// function ClozeCard {
+//    inquirer.prompt([
+  
+
+//   
+ 
+
+
+
+
+
+
